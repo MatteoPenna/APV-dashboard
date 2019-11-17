2772c4c64dfd9518b4ac66a0006d9c4b45697e78
Matteo <mattpenn360@gmail.com>
2019-11-17 13:40:59 -0500

    Updated a the command_control_server to have a fake and real one.

--- Dashboard/command_control_server.py
+++ Dashboard/command_control_server.py
@@ -2,23 +2,19 @@ import csv
 from io import StringIO
 import paho.mqtt.client as mqtt
 import serial
-import random
-import time
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
 
-    #client.subscribe('command/#')
+    client.subscribe('command/#')
 
 def on_message(client, userdata, msg):
-    '''
     if mqtt.topic_matches_sub('command/#', msg.topic):
         message_io = StringIO()
         csv.writer(message_io).writerow([msg.topic.split('/', 1)[1], msg.payload.decode('ascii')])
         ser.write(message_io.getvalue().encode('ascii'))
-    '''
 
-#ser = serial.Serial('COM8', baudrate=115200)
+ser = serial.Serial('/dev/tty/AMA0', baudrate=115200)
 
 client = mqtt.Client()
 client.on_connect = on_connect
@@ -28,29 +24,6 @@ client.connect("localhost")
 
 client.loop_start()
 
-start = time.time()
-
 while True:
-    time.sleep(1)
-    random.seed(1)
-    values = [0] * 6
-    valueName = [
-        'rpm',
-        'ultra_dist',
-        'imu',
-        'batt_temp',
-        'angular_velocity',
-        'batt_voltage']
-
-    
-    while True:
-        for i in range(0,6):
-            values[i] = random.randint(0,10)
-
-        time.sleep(0.1)
-
-        for i in range(0,6):
-            client.publish('telemetry/' + valueName[i], values[i], retain=True)
-
-        print(values)
-
+    for entry in csv.reader(StringIO(ser.readline().decode('ascii').strip())):
+        client.publish('telemetry/' + entry[0], entry[1], retain=True)
